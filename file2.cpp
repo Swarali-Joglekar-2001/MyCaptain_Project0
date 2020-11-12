7fc976eca8cccdc38ccda7ffab4154518caffcb3
Swarali-Joglekar-2001 <74260706+Swarali-Joglekar-2001@users.noreply.github.com>
2020-11-12 18:09:16 +0530

    Create file2.cpp
    Program to find the product of two numbers entered by the User

---
+++ file2.cpp
@@ -0,0 +1,19 @@
+
+
+/*Write a C++ Program to find the product of two numbers entered by the User*/ 
+/*Your Output should look something like this Enter two numbers: 3.4 5.5 Product = 18.7 */
+
+#include <iostream>
+using namespace std;
+
+/* run this program using the console pauser or add your own getch, system("pause") or input loop */
+int main(int argc, char** argv) {
+	
+	int a,b;
+	cout<<"Enter two numbers: ";
+	cin>>a>>b;
+	cout<<"Product = "<<(a*b);
+
+	
+	return 0;
+}
